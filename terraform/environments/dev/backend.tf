12dc14b830fb70c1e20370ce25dc3cfc9596cd1f
Chris Onyedinma <chrisokem@googlemail.com>
2025-09-07 21:42:13 +0100

    Add infrastructure script and workflow

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cybacad-tfstate-dev"
-    key            = "state/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "cybacad-tflock-dev"
-    encrypt        = true
+    bucket       = "cybacad-tfstate-dev"
+    key          = "state/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
