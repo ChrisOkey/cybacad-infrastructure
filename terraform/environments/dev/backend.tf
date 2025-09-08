4c39a1e9b4bae31a575bc19fb1e79df1abb7eead
Chris Onyedinma <chrisokem@googlemail.com>
2025-09-08 10:33:09 +0100

    Fix: Final correction of backend.tf file

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -4,3 +4,6 @@ bucket         = "cybacad-tfstate-dev"
 key            = "state/dev/terraform.tfstate"
 region         = "us-east-1"
 dynamodb_table = "cybacad-tflock-dev"
+encrypt        = true
+}
+}
