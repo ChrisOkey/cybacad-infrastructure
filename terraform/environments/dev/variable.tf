57d67954873bc4fe0abbedb7c34bc99406ecda5c
Chris Onyedinma <chrisokem@googlemail.com>
2025-09-10 12:59:46 +0100

    Fix Terraform formatting

--- terraform/environments/dev/variable.tf
+++ terraform/environments/dev/variable.tf
@@ -1,9 +1,9 @@
 variable "env" {
-description = "Environment name (e.g., dev, staging, prod)"
-type        = string
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
 }
 
 variable "aws_region" {
-description = "AWS region to deploy into"
-type        = string
+  description = "AWS region to deploy into"
+  type        = string
 }
