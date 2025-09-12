b4ff26a5e3e9a811703a9edc86729b8ac8872ec1
Chris Onyedinma <chrisokem@googlemail.com>
2025-09-12 17:52:09 +0100

    Final workflow and script changes

--- terraform/environments/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-s3" {
-    bucket         = "cybacad-tfstate-staging"
-    key            = "state/staging/terraform.tfstate"
-     "regterraform {
-  baion ckend        = "us-east-1"
-    dynamodb_table = "cybacad-tflock-staging"
-    encrypt        = true
-  }
-}
